1ec3d001ced385305cde0b92fed16d4b8d2b0aec
deel18 <deel18.github@gmail.com>
2020-06-01 01:51:40 +0100

    updated routes

--- models/stocks.js
+++ models/stocks.js
@@ -26,16 +26,18 @@ const stocks = {
     BuyStocks: function(res, body) {
         const email = body.email;
         const balance = body.balance;
-        const stock1 = body.stock1;
-        const stock2 = body.stock2;
-        const stock3 = body.stock3;
-        const stock4 = body.stock4;
-        const stock5 = body.stock5;
-        const stock6 = body.stock6;
-        const stock7 = body.stock7;
-        const stock8 = body.stock8;
-        const stock9 = body.stock9;
-        const stock10 = body.stock10;
+        const stock = body.stock
+        const amount = body.amount
+        // const stock1 = body.stock1;
+        // const stock2 = body.stock2;
+        // const stock3 = body.stock3;
+        // const stock4 = body.stock4;
+        // const stock5 = body.stock5;
+        // const stock6 = body.stock6;
+        // const stock7 = body.stock7;
+        // const stock8 = body.stock8;
+        // const stock9 = body.stock9;
+        // const stock10 = body.stock10;
 
         if (!email) {
             return res.status(401).json({
@@ -48,7 +50,7 @@ const stocks = {
             })
         };
         //add stocks to the users depot
-        db.run("UPDATE users SET balance = ?, stock1 = ?, stock2 = ?, stock3 = ?, stock4 = ?, stock5 = ?, stock6 =?, stock7 = ?, stock8 = ?, stock9 = ?, stock10 = ? WHERE email = ?", balance, stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9, stock10, email, function(err) {
+        db.run("UPDATE users SET balance = ?, " + stock + " = ?" + "WHERE email = ?", balance, amount, email, function(err) {
             if (err) {
                 return res.status(500).json({
                     error: {
